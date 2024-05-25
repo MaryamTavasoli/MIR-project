df5e0b79b2d7f49510c5fd244eab3c8ca5d69b2f
MaryamTavasoli <110398642+MaryamTavasoli@users.noreply.github.com>
2024-05-25 20:12:10 +0330

    Update search.py

--- Logic/core/search.py
+++ Logic/core/search.py
@@ -1,8 +1,7 @@
 import json
-from .preprocess import Preprocessor
-from .scorer import Scorer
-from .indexes_enum import Indexes, Index_types
-from .index_reader import Index_reader
+import numpy as np
+from .utility import Preprocessor, Scorer
+from .indexer import Indexes, Index_types, Index_reader
 
 
 class SearchEngine:
@@ -11,25 +10,45 @@ class SearchEngine:
         Initializes the search engine.
 
         """
-        path = '/index'
+        path = "/index"
         self.document_indexes = {
             Indexes.STARS: Index_reader(path, Indexes.STARS),
             Indexes.GENRES: Index_reader(path, Indexes.GENRES),
-            Indexes.SUMMARIES: Index_reader(path, Indexes.SUMMARIES)
+            Indexes.SUMMARIES: Index_reader(path, Indexes.SUMMARIES),
         }
         self.tiered_index = {
             Indexes.STARS: Index_reader(path, Indexes.STARS, Index_types.TIERED),
             Indexes.GENRES: Index_reader(path, Indexes.GENRES, Index_types.TIERED),
-            Indexes.SUMMARIES: Index_reader(path, Indexes.SUMMARIES, Index_types.TIERED)
+            Indexes.SUMMARIES: Index_reader(
+                path, Indexes.SUMMARIES, Index_types.TIERED
+            ),
         }
         self.document_lengths_index = {
-            Indexes.STARS: Index_reader(path, Indexes.STARS, Index_types.DOCUMENT_LENGTH),
-            Indexes.GENRES: Index_reader(path, Indexes.GENRES, Index_types.DOCUMENT_LENGTH),
-            Indexes.SUMMARIES: Index_reader(path, Indexes.SUMMARIES, Index_types.DOCUMENT_LENGTH)
+            Indexes.STARS: Index_reader(
+                path, Indexes.STARS, Index_types.DOCUMENT_LENGTH
+            ),
+            Indexes.GENRES: Index_reader(
+                path, Indexes.GENRES, Index_types.DOCUMENT_LENGTH
+            ),
+            Indexes.SUMMARIES: Index_reader(
+                path, Indexes.SUMMARIES, Index_types.DOCUMENT_LENGTH
+            ),
         }
-        self.metadata_index = Index_reader(path, Indexes.DOCUMENTS, Index_types.METADATA)
+        self.metadata_index = Index_reader(
+            path, Indexes.DOCUMENTS, Index_types.METADATA
+        )
 
-    def search(self, query, method, weights, safe_ranking=True, max_results=10):
+    def search(
+        self,
+        query,
+        method,
+        weights,
+        safe_ranking=True,
+        max_results=10,
+        smoothing_method=None,
+        alpha=0.5,
+        lamda=0.5,
+    ):
         """
         searches for the query in the indexes.
 
@@ -37,7 +56,7 @@ class SearchEngine:
         ----------
         query : str
             The query to search for.
-        method : str ((n|l)(n|t)(n|c).(n|l)(n|t)(n|c)) | OkapiBM25
+        method : str ((n|l)(n|t)(n|c).(n|l)(n|t)(n|c)) | OkapiBM25 | Unigram
             The method to use for searching.
         weights: dict
             The weights of the fields.
@@ -46,21 +65,33 @@ class SearchEngine:
             If False, the search engine will search in tiered index.
         max_results : int
             The maximum number of results to return. If None, all results are returned.
+        smoothing_method : str (bayes | naive | mixture)
+            The method used for smoothing the probabilities in the unigram model.
+        alpha : float, optional
+            The parameter used in bayesian smoothing method. Defaults to 0.5.
+        lamda : float, optional
+            The parameter used in some smoothing methods to balance between the document
+            probability and the collection probability. Defaults to 0.5.
 
         Returns
         -------
         list
             A list of tuples containing the document IDs and their scores sorted by their scores.
         """
-
         preprocessor = Preprocessor([query])
-        query = preprocessor.preprocess()[0].split()
+        query = preprocessor.preprocess()[0]
 
         scores = {}
-        if safe_ranking:
+        if method == "unigram":
+            self.find_scores_with_unigram_model(
+                query, smoothing_method, weights, scores, alpha, lamda
+            )
+        elif safe_ranking:
             self.find_scores_with_safe_ranking(query, method, weights, scores)
         else:
-            self.find_scores_with_unsafe_ranking(query, method, weights, max_results, scores)
+            self.find_scores_with_unsafe_ranking(
+                query, method, weights, max_results, scores
+            )
 
         final_scores = {}
 
@@ -87,11 +118,14 @@ class SearchEngine:
         """
         # TODO
         for field in weights:
-            for document_id in scores[field]:
-                final_scores[document_id] = final_scores.get(document_id, 0) + weights[field] * scores[field][
-                    document_id]
+            if scores:
+                for document_id in scores[field]:
+                    final_scores[document_id] = final_scores.get(document_id, 0) + weights[field] * scores[field][
+                        document_id]
 
-    def find_scores_with_unsafe_ranking(self, query, method, weights, max_results, scores):
+    def find_scores_with_unsafe_ranking(
+        self, query, method, weights, max_results, scores
+    ):
         """
         Finds the scores of the documents using the unsafe ranking method using the tiered index.
 
@@ -114,31 +148,32 @@ class SearchEngine:
         for field in weights:
             for tier in ["first_tier", "second_tier", "third_tier"]:
                 # TODO
-                scorer = Scorer(self.tiered_index[field][tier], self.metadata_index['document_count'])
+                scorer = Scorer(self.tiered_index[field].index[tier], self.metadata_index.index['document_count'])
                 if method == 'OkapiBM25':
                     average_document_length = self.metadata_index["average_document_length"][field.value]
                     document_length = self.document_lengths_index[field]
                     if tier == "first_tier":
-                        scores1[field].update(
-                            scorer.compute_socres_with_okapi_bm25(query, average_document_length, document_length))
+                        scores1[field] = scorer.compute_socres_with_okapi_bm25(query, average_document_length,
+                                                                               document_length)
                     elif tier == "second_tier":
-                        scores2[field].update(
-                            scorer.compute_socres_with_okapi_bm25(query, average_document_length, document_length))
+                        scores2[field] = scorer.compute_socres_with_okapi_bm25(query, average_document_length,
+                                                                               document_length)
                     else:
-                        scores3[field].update(
-                            scorer.compute_socres_with_okapi_bm25(query, average_document_length, document_length))
+                        scores3[field] = scorer.compute_socres_with_okapi_bm25(query, average_document_length,
+                                                                               document_length)
                 else:
                     if tier == "first_tear":
-                        scores1[field].update(scorer.compute_scores_with_vector_space_model(query, method))
+                        scores1[field] = scorer.compute_scores_with_vector_space_model(query, method)
                     elif tier == "second_tear":
-                        scores2[field].update(scorer.compute_scores_with_vector_space_model(query, method))
+                        scores2[field] = scorer.compute_scores_with_vector_space_model(query, method)
                     else:
-                        scores3[field].update(scorer.compute_scores_with_vector_space_model(query, method))
-                scores = self.merge_scores(scores1, scores2, field)
-                scores = self.merge_scores(scores, scores3, field)
+                        scores3[field] = scorer.compute_scores_with_vector_space_model(query, method)
+                if scores1 and scores2:
+                    scores = self.merge_scores(scores1, scores2, field)
+                if scores and scores3:
+                    scores = self.merge_scores(scores, scores3, field)
                 if max_results != -1 and (len(scores)) > max_results:
                     break
-
     def find_scores_with_safe_ranking(self, query, method, weights, scores):
         """
         Finds the scores of the documents using the safe ranking method.
@@ -157,14 +192,55 @@ class SearchEngine:
 
         for field in weights:
             # TODO
-            scorer = Scorer(self.metadata_index['document_count'])
+            field1 = ""
+            if field == Indexes.STARS:
+                field1 = 'stars'
+            elif field == Indexes.GENRES:
+                field1 = 'genres'
+            else:
+                field1 = 'summaries'
+            scorer = Scorer(self.document_indexes[field].index[field1], self.metadata_index.index['document_count'])
             if method == 'OkapiBM25':
                 average_document_length = self.metadata_index["average_document_length"][field.value]
                 document_length = self.document_lengths_index[field]
-                scores[field].update(
-                    scorer.compute_socres_with_okapi_bm25(query, average_document_length, document_length))
+                scores[field] = scorer.compute_socres_with_okapi_bm25(query, average_document_length, document_length)
             else:
-                scores[field].update(scorer.compute_scores_with_vector_space_model(query, method))
+                scores[field] = scorer.compute_scores_with_vector_space_model(query, method)
+    def find_scores_with_unigram_model(
+        self, query, smoothing_method, weights, scores, alpha=0.5, lamda=0.5
+    ):
+        """
+        Calculates the scores for each document based on the unigram model.
+
+        Parameters
+        ----------
+        query : str
+            The query to search for.
+        smoothing_method : str (bayes | naive | mixture)
+            The method used for smoothing the probabilities in the unigram model.
+        weights : dict
+            A dictionary mapping each field (e.g., 'stars', 'genres', 'summaries') to its weight in the final score. Fields with a weight of 0 are ignored.
+        scores : dict
+            The scores of the documents.
+        alpha : float, optional
+            The parameter used in bayesian smoothing method. Defaults to 0.5.
+        lamda : float, optional
+            The parameter used in some smoothing methods to balance between the document
+            probability and the collection probability. Defaults to 0.5.
+        """
+        # TODO
+        for field in weights:
+            # TODO
+            field1 = ""
+            if field == Indexes.STARS:
+                field1 = 'stars'
+            elif field == Indexes.GENRES:
+                field1 = 'genres'
+            else:
+                field1 = 'summaries'
+            scorer = Scorer(self.document_indexes[field].index[field1], self.metadata_index.index['document_count'])
+            document_length = self.document_lengths_index[field]
+            scores[field] = scorer.compute_scores_with_unigram_model(query,smoothing_method,document_length,alpha,lamda)
 
     def merge_scores(self, scores1, scores2, field):
         """
@@ -192,15 +268,11 @@ class SearchEngine:
         return merged_scores
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     search_engine = SearchEngine()
     query = "spider man in wonderland"
     method = "lnc.ltc"
-    weights = {
-        Indexes.STARS: 1,
-        Indexes.GENRES: 1,
-        Indexes.SUMMARIES: 1
-    }
+    weights = {Indexes.STARS: 1, Indexes.GENRES: 1, Indexes.SUMMARIES: 1}
     result = search_engine.search(query, method, weights)
 
     print(result)
